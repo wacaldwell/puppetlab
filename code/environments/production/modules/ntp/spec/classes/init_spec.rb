83399e0f5b7f76d30eaf7bb4aed359d30e344d38
Alex Caldwell <wacaldwell@gmail.com>
2020-05-04 15:28:50 -0700

    more master cleanup

--- code/environments/production/modules/ntp/spec/classes/init_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-describe 'ntp' do
-  context 'with defaults for all parameters' do
-     let(:facts) {{ :kernel => 'linux' }}
-    it { should contain_class('ntp') }
-    it { should contain_class('ntp::install') }
-  end
-end
